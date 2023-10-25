0a88423987af81571e543d8e32c647631603d329
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2023-10-25 14:32:02 -0400

    Update myTreeIterator.java

--- src/myTreeIterator.java
+++ src/myTreeIterator.java
@@ -1,6 +1,6 @@
 
 /**
- * myTreeIterator - 
+ * myTreeIterator - A Class that implements ITreeIterator
  * @author Nathaniel Serrano
  * @version 25 October 2023
  *
@@ -10,6 +10,10 @@ public class myTreeIterator implements ITreeIterator{
 	private int sequenceArray[];
 	private int nextIndex;
 	
+	/**
+	 * myTreeIterator()-- Constructor --Takes in a BinarySearchTree
+	 * @param bst - A Binary Search Tree.
+	 */
 	public myTreeIterator(BinarySearchTree<Integer> bst) {
 		sequenceArray = new int[bst.size];
 		nextIndex = 0;
@@ -17,12 +21,19 @@ public class myTreeIterator implements ITreeIterator{
 		nextIndex = 0;
 	}
 
-	
+	/**
+	 * SequenceArray() - Returns all values of the tree left to right in an array.
+	 * @param bst - A Binary Search Tree
+	 */
 	public int[] SequenceArray(BinarySearchTree<Integer> bst) {
 		buildSequence(bst.root);
 		return sequenceArray;
 	}
 	
+	/**
+	 * buildSequence() - Helper method of Sequence Array
+	 * @param node - Current local root
+	 */
 	private void buildSequence(BinarySearchTree.Node node) {
 		if (node==null)
 			return;
